0e7cae96742599717091928429cf1cd7843d4132
elsehow <yes@cosmopol.is>
2015-12-12 10:44:52 -0800

    refactored Rate

--- patches/Rate.js
+++ patches/Rate.js
@@ -1,15 +1,8 @@
 // returns a stream of rate 
 // in number of vals per s
 function rate (s) {
-  var count = 0
-  var time = 1000
-  s.onValue(_ => count += 1)
-  return Kefir.stream(emitter => {
-    setInterval(_ => {
-      emitter.emit(count)
-      count = 0
-    }, time)
-  })
+  s.bufferBy(Kefir.interval(1000))
+    .map(buff => buff.length)
 }
 
 module.exports = rate
